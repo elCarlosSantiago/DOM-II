eec0ebbcf64915127d0965753cdb18aadab05da0
CarlosSantiago Nevett <csnevett@gmail.com>
2021-01-13 09:46:23 -0500

    finished mvp

--- js/index.js
+++ js/index.js
@@ -6,8 +6,15 @@ const navLinks = Array.from(document.querySelectorAll(".nav-link"));
 const funLogo = document.querySelector(".logo-heading");
 
 navLinks.forEach((elem) => {
-  elem.addEventListener("mouseover", () => {
+  elem.addEventListener("mouseenter", () => {
     elem.style.color = "blue";
+  });
+});
+
+navLinks.forEach((elem) => {
+  elem.addEventListener("mouseleave", () => {
+    elem.style.color = "black";
+    event.preventDefault();
   });
 });
 
@@ -30,19 +37,53 @@ window.addEventListener("load", () => {
 const funBus = document.querySelector(".intro img");
 
 funBus.addEventListener("dblclick", (e) => {
-    funBus.style.borderRadius = '50%';
+  funBus.style.borderRadius = "50%";
 });
 
-const welcome = document.querySelector('.intro h2');
+const welcome = document.querySelector(".intro h2");
 
-welcome.addEventListener('copy', () => {
-    welcome.style.color = 'red';
-})
+welcome.addEventListener("copy", () => {
+  welcome.style.color = "red";
+});
 
-const letGo = document.querySelector('.content-section .text-content');
+const letGo = document.querySelector(".content-section .text-content");
 
-document.addEventListener('keyup', (e) => {
-   if (e.key === 'Backspace'){
-       letGo.textContent = 'OH GOD YOU DELETED ALL MY TEXT OH MY GOD IT HURTS INSIDE!'
-   }
-})
+document.addEventListener("keyup", (e) => {
+  if (e.key === "Backspace") {
+    letGo.textContent =
+      "OH GOD YOU DELETED ALL MY TEXT OH MY GOD IT HURTS INSIDE!";
+  }
+});
+
+const contImgs = document.querySelectorAll(".content-section img");
+
+contImgs.forEach((elem) => {
+  elem.addEventListener("mouseover", () => {
+    elem.style.opacity = "0.5";
+  });
+});
+
+const adv = document.querySelector(".inverse-content .text-content h2");
+
+adv.addEventListener("cut", () => {
+  adv.textContent = "Im too adventurous to be cut lmao leave sir";
+});
+
+const btns = document.querySelectorAll(".btn");
+
+btns.forEach((elem) => {
+  elem.addEventListener("click", () => {
+    alert("YOU CANT REALLY SIGN UP SIR THIS PAGE IS FAKE");
+    event.stopPropagation();
+  });
+});
+
+const destination = document.querySelectorAll(".destination");
+
+console.log(destination)
+
+destination.forEach((elem) => {
+  elem.addEventListener("click", () => {
+      elem.style.backgroundColor = 'yellow';
+  });
+});
