abed49aebd0e797f93e3d0f7133ba02cbce3fff2
CarlosSantiago Nevett <csnevett@gmail.com>
2021-01-12 19:20:04 -0500

    working with some event listeners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,34 @@
 // Your code goes here
+// Nav Stuff
+const header = document.querySelector(".main-navigation");
+const nav = document.querySelector(".nav");
+const navLinks = Array.from(document.querySelectorAll(".nav-link"));
+const funLogo = document.querySelector(".logo-heading");
+
+navLinks.forEach((elem) => {
+  elem.addEventListener("mouseover", () => {
+    elem.style.color = "blue";
+  });
+});
+
+document.addEventListener("keydown", (e) => {
+  if (e.key === "Escape") {
+    header.style.position = "static";
+  }
+});
+
+document.addEventListener("wheel", () => {
+  nav.style.backgroundColor = "red";
+});
+
+window.addEventListener("load", () => {
+  funLogo.style.fontSize = "10rem";
+});
+
+// Container home main content
+
+const funBus = document.querySelector(".intro img");
+
+funBus.addEventListener("dblclick", (e) => {
+    funBus.style.borderRadius = '50%';
+});
